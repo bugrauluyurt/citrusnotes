28091e9f2d12a7bb3ec04579661aa667da85e972
Bugra Uluyurt <bugra.u@gmail.com>
2020-01-30 09:46:02 -0500

    Handler file added

---
+++ src/shared/store/epicResponseHandler.ts
@@ -0,0 +1,7 @@
+import { Action } from 'store/app/types';
+export const epicResponseHandler = (
+    response: any | Error,
+    actions: Array<(payload: any) => Action>
+): Action => {
+    return response instanceof Error ? actions[1](response.message) : actions[0](response);
+};
