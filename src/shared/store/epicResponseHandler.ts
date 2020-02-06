a56f50e7d87f463128f13b6f6903df94d629a7c2
Bugra Uluyurt <bugra.u@gmail.com>
2020-02-05 19:37:07 -0500

    Error handling update

--- src/shared/store/epicResponseHandler.ts
+++ src/shared/store/epicResponseHandler.ts
@@ -3,5 +3,5 @@ export const epicResponseHandler = (
     response: any | Error,
     actions: Array<(payload: any) => Action>
 ): Action => {
-    return response instanceof Error ? actions[1](response.message) : actions[0](response);
+    return !(response instanceof Error) ? actions[0](response) : actions[1](response.message);
 };
